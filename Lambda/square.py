dc7e52bdf24af044539cea649fb1f0b7f13034ba
Sandesh-Hase <sandeshhase15@gmail.com>
2022-11-18 00:20:52 +0530

    square

--- Lambda/square.py
+++ Lambda/square.py
@@ -1,4 +1,10 @@
 
+'''
+Lambda is a anonymous function, means we no need to define it
+We can perform only one operation at a time
+We can pass multiple arguments at a time
+'''
+
 f = lambda a:a*a
 square=f(6)
 
